0bae5c2b23eb52e8888541770bb88ec1206bf5a9
Weibin Liang <weliang@redhat.com>
2022-12-05 18:16:44 -0500

    Update networking.rb

--- features/step_definitions/networking.rb
+++ features/step_definitions/networking.rb
@@ -1,6 +1,6 @@
 Given /^the joint network CIDR is updateded in the node "([^"]*)"$/ do | node_name |
   ensure_admin_tagged
-  @result = admin.cli_exec(:get, resource: "node/#{node_name}", output: "jsonpath={.metadata.annotations.k8s.ovn.org/node-gateway-router-lrp-ifaddr}")
+  @result = admin.cli_exec(:get, resource: "node/#{node_name}", output: "jsonpath={.metadata.annotations.k8s\.ovn\.org/node-gateway-router-lrp-ifaddr}")
   def_inf = @result[:response]
   logger.info "The node's default interface is #{def_inf}"
   unless @result[:response].include? "100.66.0"
